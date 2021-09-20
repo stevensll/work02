3ab04be726409c22ab3e712202428a6bd0b640f0
stevensll <deltaorb637@gmail.com>
2021-09-20 00:03:17 -0400

    testing

--- work2.c
+++ work2.c
@@ -6,5 +6,6 @@ int main(){
     int a;
     printf("These are types: %d %lf %f %ld %c %s %p", 3, 4.5, 213123.23232323, 2323223123221,'c', "hi", &a);
     printf("This might break: %c", '21903');
+    printf("hello")
     return 0;
 }
